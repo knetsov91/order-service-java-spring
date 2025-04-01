50279f7bde4cf1b9725957c7de3fc09e744068d3
Konstantin <knetsov91@gmail.com>
2025-04-02 01:17:14 +0300

    Add POST "/api/v1/orders" in OrderController to create order.

--- src/main/java/restaurant/com/orderservice/order/repository/OrderRepository.java
+++ src/main/java/restaurant/com/orderservice/order/repository/OrderRepository.java
@@ -1,22 +1,16 @@
 package restaurant.com.orderservice.order.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import restaurant.com.orderservice.order.model.Order;
-import restaurant.com.orderservice.order.model.OrderStatus;
-import restaurant.com.orderservice.web.dto.OrderResponse;
 
-import javax.swing.text.html.Option;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
 
-    Optional<List<Order>> findByWaiter(UUID orderId);
+//    Optional<List<Order>> findByWaiter(UUID orderId);
 
-    @Query("FROM Order o WHERE o.orderStatus = :status")
-    List<Order> getOrderByStatus(OrderStatus status);
+//    @Query("FROM Order o WHERE o.orderStatus = :status")
+//    List<Order> getOrderByStatus(OrderStatus status);
 }

f7d645568cbfb025bf9bbb025e69456a96f119e7
Konstantin <knetsov91@gmail.com>
2025-04-01 22:14:32 +0300

    Add mapOrderListToOrderResponseList() in DtoMapper.

--- src/main/java/restaurant/com/orderservice/web/mapper/DtoMapper.java
+++ src/main/java/restaurant/com/orderservice/web/mapper/DtoMapper.java
@@ -5,6 +5,9 @@ import restaurant.com.orderservice.orderInfo.OrderInfo;
 import restaurant.com.orderservice.web.dto.OrderInfoResponse;
 import restaurant.com.orderservice.web.dto.OrderResponse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DtoMapper {
 
     public static OrderInfoResponse mapOrderInfoToOrderInfoResponse(OrderInfo order) {
@@ -26,6 +29,18 @@ public class DtoMapper {
         orderResponse.setClientId(order.getClientId());
         orderResponse.setRestaurantId(order.getRestaurantId());
 
+        return orderResponse;
+    }
+
+    public static OrderResponse mapOrderListToOrderResponseList(Order order, List<OrderInfo> orderInfoList) {
+        List<OrderInfoResponse> orderResponseList = new ArrayList<>();
+        for (OrderInfo orderInfo : orderInfoList) {
+            OrderInfoResponse orderInfoResponse = mapOrderInfoToOrderInfoResponse(orderInfo);
+            orderResponseList.add(orderInfoResponse);
+        }
+        OrderResponse orderResponse = mapOrderToOrderResponse(order);
+
+        orderResponse.setOrderInfoResponses(orderResponseList);
         return orderResponse;
     }
 }

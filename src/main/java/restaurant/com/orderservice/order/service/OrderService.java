cf776f12d8ebc1afa29b947998116af9b7ac5cb5
Konstantin <knetsov91@gmail.com>
2025-04-02 01:47:38 +0300

    Add completeOrder() in OrderService.

--- src/main/java/restaurant/com/orderservice/order/service/OrderService.java
+++ src/main/java/restaurant/com/orderservice/order/service/OrderService.java
@@ -4,75 +4,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import restaurant.com.orderservice.exception.OrderNotFoundException;
+import restaurant.com.orderservice.factory.OrderFactory;
 import restaurant.com.orderservice.order.model.Order;
 import restaurant.com.orderservice.order.model.OrderStatus;
 import restaurant.com.orderservice.order.repository.OrderRepository;
-import restaurant.com.orderservice.web.dto.CreateOrderRequest;
-import restaurant.com.orderservice.web.dto.OrderResponse;
-import restaurant.com.orderservice.web.mapper.DtoMapper;
-
+import restaurant.com.orderservice.orderInfo.service.OrderInfoService;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class OrderService {
     private final Logger LOGGER = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
+    private OrderFactory orderFactory;
+    private OrderInfoService orderInfoService;
 
     @Autowired
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, OrderFactory orderFactory, OrderInfoService orderInfoService) {
         this.orderRepository = orderRepository;
+        this.orderFactory = orderFactory;
+        this.orderInfoService = orderInfoService;
     }
 
-    public List<OrderResponse> getAllOrders() {
-        return orderRepository
-                .findAll()
-                .stream()
-                .map(DtoMapper::mapOrderToOrderResponse)
-                .toList();
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
     }
-
-    public UUID createOrder(CreateOrderRequest createOrderRequest) {
-
-        Order order = DtoMapper.mapOrderToOrderResponse(createOrderRequest);
-        orderRepository.save(order);
-        LOGGER.info("Order {} created: {}", order.getId(), order.getCreatedAt());
-        return order.getId();
-    }
-
-    public List<OrderResponse> getWaiterOrders(UUID waiterId) {
-
-        Optional<List<Order>> orders = orderRepository.findByWaiter(waiterId);
-        if (orders.isEmpty()) {
-            throw new RuntimeException("Orders for [%s] not found".formatted(waiterId));
-        }
-        return orders
-                .get()
-                .stream()
-                .map(DtoMapper::mapOrderToOrderResponse)
-                .toList();
-
-    }
-
-    public void changeOrderStatus(UUID orderId, OrderStatus orderStatus) {
-        Optional<Order> orderOptional = orderRepository.findById(orderId);
-
-        if (orderOptional.isEmpty()) {
-            LOGGER.error("Order with id [%s] not found".formatted(orderId));
-            throw new OrderNotFoundException("Order with id [%s] not found".formatted(orderId));
-        }
-
-        Order order = orderOptional.get();
-        order.setOrderStatus(orderStatus);
-        LOGGER.info("Order with id [%s] updated to %s status".formatted(orderId, orderStatus));
+    public void completeOrder(Long orderId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new RuntimeException("Order with id " + orderId + " not found"));
+        order.setFinishDate(LocalDateTime.now());
+        order.setOrderStatus(OrderStatus.COMPLETED);
 
         orderRepository.save(order);
-    }
-
-    public List<Order> getOrdersByStatus(OrderStatus orderStatus) {
-        List<Order> orderByStatus = orderRepository.getOrderByStatus(orderStatus);
-        return orderByStatus;
     }
 }

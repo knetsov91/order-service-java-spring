2ae51d298edbd52272bb52b9441c961a74b74ed5
Konstantin <knetsov91@gmail.com>
2025-04-01 22:13:46 +0300

    Add mapOrderToOrderResponse() in DtoMapper.

--- src/main/java/restaurant/com/orderservice/web/mapper/DtoMapper.java
+++ src/main/java/restaurant/com/orderservice/web/mapper/DtoMapper.java
@@ -1,7 +1,9 @@
 package restaurant.com.orderservice.web.mapper;
 
+import restaurant.com.orderservice.order.model.Order;
 import restaurant.com.orderservice.orderInfo.OrderInfo;
 import restaurant.com.orderservice.web.dto.OrderInfoResponse;
+import restaurant.com.orderservice.web.dto.OrderResponse;
 
 public class DtoMapper {
 
@@ -12,5 +14,18 @@ public class DtoMapper {
         orderInfoResponse.setMenuItemId(order.getMenuItemId());
 
         return orderInfoResponse;
+    }
+
+    public static OrderResponse mapOrderToOrderResponse(Order order) {
+        OrderResponse orderResponse = new OrderResponse();
+        orderResponse.setId(order.getId());
+        orderResponse.setOrderDate(order.getOrderDate());
+        orderResponse.setOrderStatus(order.getOrderStatus());
+        orderResponse.setFinishDate(order.getFinishDate());
+        orderResponse.setWaiterId(order.getWaiterId());
+        orderResponse.setClientId(order.getClientId());
+        orderResponse.setRestaurantId(order.getRestaurantId());
+
+        return orderResponse;
     }
 }

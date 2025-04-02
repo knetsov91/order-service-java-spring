e9fcb404c7f6f2c9a77fc6b866de8d5174558eb7
Konstantin <knetsov91@gmail.com>
2025-04-02 15:35:29 +0300

    Add findByRestaurantId() in OrderRepository.

--- src/main/java/restaurant/com/orderservice/order/repository/OrderRepository.java
+++ src/main/java/restaurant/com/orderservice/order/repository/OrderRepository.java
@@ -3,14 +3,10 @@ package restaurant.com.orderservice.order.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import restaurant.com.orderservice.order.model.Order;
-
-import java.util.UUID;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, UUID> {
-
-//    Optional<List<Order>> findByWaiter(UUID orderId);
-
-//    @Query("FROM Order o WHERE o.orderStatus = :status")
-//    List<Order> getOrderByStatus(OrderStatus status);
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<List<Order>> findByRestaurantId(Long restaurantId);
 }

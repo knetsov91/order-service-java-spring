6f724b75ed2253be4409e48be4e062adf09d3b20
Konstantin <knetsov91@gmail.com>
2025-04-02 16:13:58 +0300

    Add findByWaiterId() in OrderRepository.

--- src/main/java/restaurant/com/orderservice/order/repository/OrderRepository.java
+++ src/main/java/restaurant/com/orderservice/order/repository/OrderRepository.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Repository;
 import restaurant.com.orderservice.order.model.Order;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Optional<List<Order>> findByRestaurantId(Long restaurantId);
+    Optional<List<Order>> findByWaiterId(UUID waiterId);
 }

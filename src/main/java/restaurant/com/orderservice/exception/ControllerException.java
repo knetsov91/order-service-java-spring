c852e0c6249e306f3eab94364aec9e5a6cc0c9c8
Konstantin <knetsov91@gmail.com>
2025-04-02 15:48:02 +0300

    Add exception handling when change order status with invalid.

--- src/main/java/restaurant/com/orderservice/exception/ControllerException.java
+++ src/main/java/restaurant/com/orderservice/exception/ControllerException.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,6 +17,15 @@ import java.util.List;
 @Slf4j
 @ControllerAdvice
 public class ControllerException {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleChangeOrderStatusException(HttpMessageNotReadableException e) {
+        log.error(e.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse("Invalid order status", 400);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)

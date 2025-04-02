b8ce9b107765c9484f816e5293785dce0e6b5839
Konstantin <knetsov91@gmail.com>
2025-04-02 16:31:36 +0300

    Add restaurantNotFound() exception handler in ControllerException.

--- src/main/java/restaurant/com/orderservice/exception/ControllerException.java
+++ src/main/java/restaurant/com/orderservice/exception/ControllerException.java
@@ -46,6 +46,16 @@ public class ControllerException {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(RestaurantNotFoundException.class)
+    public ResponseEntity<ErrorResponse> restaurantNotFound(HttpServletRequest request, RestaurantNotFoundException e) {
+        log.error("Exception in %s".formatted(request.getRequestURI()));
+        log.error(e.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), 404);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> generalHandle(HttpServletRequest request, Exception e) {

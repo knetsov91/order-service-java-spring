7927c9f455342b372a8ae69a829e238b04e40b02
Konstantin <knetsov91@gmail.com>
2025-04-02 09:26:16 +0300

    Add messages to validations in CreateOrderRequest DTO.

--- src/main/java/restaurant/com/orderservice/web/dto/CreateOrderRequest.java
+++ src/main/java/restaurant/com/orderservice/web/dto/CreateOrderRequest.java
@@ -1,5 +1,7 @@
 package restaurant.com.orderservice.web.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,13 +17,18 @@ public class CreateOrderRequest {
 
     private UUID waiterId;
 
+    @NotNull(message = "Restaurant Id is required.")
     private Long restaurantId;
 
     private UUID clientId;
 
+    @NotNull(message = "Price is required.")
     private BigDecimal price;
 
+    @NotNull(message = "Menu item Id is required.")
     private UUID menuItemId;
 
+    @NotNull
+    @Positive(message = "Quantity must be greater then zero.")
     private int quantity;
 }

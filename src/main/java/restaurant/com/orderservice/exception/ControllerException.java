f01f4f342e7800e8d894adcfe903e96e0cc58c8d
Konstantin <knetsov91@gmail.com>
2025-04-02 09:30:29 +0300

    Add handleDtoValidation() in ControllerException.

--- src/main/java/restaurant/com/orderservice/exception/ControllerException.java
+++ src/main/java/restaurant/com/orderservice/exception/ControllerException.java
@@ -4,11 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import restaurant.com.orderservice.web.dto.ErrorResponse;
+import java.util.List;
 
 @Slf4j
 @ControllerAdvice
@@ -18,7 +21,16 @@ public class ControllerException {
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public void handle(HttpServletRequest request) {
         log.error("MethodArgumentTypeMismatchException %s".formatted(request.getRequestURI()));
+    }
 
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<ErrorResponse> orderNotFound(HttpServletRequest request, OrderNotFoundException e) {
+        log.error("Exception in %s".formatted(request.getRequestURI()));
+        log.error(e.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse("Order not found", 404);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
@@ -30,13 +42,21 @@ public class ControllerException {
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    @ExceptionHandler(OrderNotFoundException.class)
-    public ResponseEntity<ErrorResponse> orderNotFound(HttpServletRequest request, OrderNotFoundException e) {
-        log.error("Exception in %s".formatted(request.getRequestURI()));
-        log.error(e.getMessage());
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleDtoValidation(HttpServletRequest request, MethodArgumentNotValidException e, BindingResult bindingResult) {
+        List<String> allErrors = bindingResult.getAllErrors()
+                .stream()
+                .map( error -> error.getDefaultMessage())
+                .toList();
 
-        ErrorResponse errorResponse = new ErrorResponse("Order not found", 404);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        String errorMessages = String.join("\n", allErrors);
+
+        log.error("Exception in %s".formatted(request.getRequestURI()));
+        log.error(errorMessages);
+
+        ErrorResponse errorResponse = new ErrorResponse(errorMessages, 400);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }

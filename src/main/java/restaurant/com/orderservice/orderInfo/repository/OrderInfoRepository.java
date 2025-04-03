a1cfacb8ebe9e2ea6bca5fc5641bb5bc76c344d8
Konstantin <knetsov91@gmail.com>
2025-04-03 19:17:21 +0300

    Add OrderInfoRepository.

--- src/main/java/restaurant/com/orderservice/orderInfo/repository/OrderInfoRepository.java
+++ src/main/java/restaurant/com/orderservice/orderInfo/repository/OrderInfoRepository.java
@@ -2,7 +2,7 @@ package restaurant.com.orderservice.orderInfo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import restaurant.com.orderservice.orderInfo.OrderInfo;
+import restaurant.com.orderservice.orderInfo.model.OrderInfo;
 
 @Repository
 public interface OrderInfoRepository extends JpaRepository<OrderInfo, Long> {
